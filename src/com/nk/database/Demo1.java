4ca71a8c4e1b66cd8363402a7c08146c036d2319
NikiMevada <mevada.niki2112@gmail.com>
2018-11-12 22:41:03 +0300

    remove space

--- src/com/nk/database/Demo1.java
+++ src/com/nk/database/Demo1.java
@@ -13,7 +13,6 @@ public class Demo1 {
 			ResultSet rs=stm.executeQuery("select *from t1");
 			while(rs.next()){
 				System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3));
-				
 			}
 			con.close();
 			}catch(Exception e){
